174706c8f78a4a6d441f07460cafec48896826f2
T0KK3N <daniel.mora.ortiz@udla.edu.ec>
2021-06-11 20:01:13 -0500

    3 files modified

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-import './style.css';
+import $ from 'jquery';
 
-$(document).ready(function() {
-  $('#button').click(function() {
-    var toAdd = $('input[name=itemCompras]').val();
-    $('ol').append('<li>' + toAdd + '</li>');
-    $('input').val('');
-  });
+$('#button').click(function() {
+  var toAdd = $('input[name=itemCompras]').val();
+  $('ol').append('<li>' + toAdd + '</li>');
+  $('input').val('');
+});
 
-  $('input[name=itemCompras]').keyup(function(event) {
-    if (event.keyCode == 13) {
-      $('#button').click();
-    }
-  });
+$('input[name=itemCompras]').keyup(function(event) {
+  if (event.keyCode == 13) {
+    $('#button').click();
+  }
+});
 
-  $('form').on('submit', function(e) {
-    e.preventDefault();
-  });
+$('form').on('submit', function(e) {
+  e.preventDefault();
 });
